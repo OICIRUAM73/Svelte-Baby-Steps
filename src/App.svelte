5e5be637208ad1babfe6e7a5a04551ad7a4f5ba2
Mauricio Llumiquinga <oiciruam73@hotmail.com>
2020-05-11 21:08:07 -0500

    Render by condition

--- src/App.svelte
+++ src/App.svelte
@@ -34,7 +34,10 @@
 		--theme-color: purple;
 	}
 
-
+	:global(body.dark-mode) {
+		background-color: #1d3040;
+		color: #bfc2c7;
+	}
 
 	main {
 		text-align: center;
--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,15 @@
 <script>
   let someText = `Frontend Developer at Listatree`;
   let count = 0;
+  let styles = {darkMode: false};
 
   function handleClick() {
     count += 1;
+  }
+
+  function toggleDarkMode() {
+    styles.darkMode = !styles.darkMode;
+    window.document.body.classList.toggle("dark-mode");
   }
 
 </script>
@@ -16,8 +22,13 @@
 </style>
 
 <div class="About">
-  <p>
-    {someText}
-  </p>
+  {#if !styles.darkMode}  
+    <p>
+      {someText}
+    </p>
+  {:else}
+    <p>Hello Dark Mode</p>
+  {/if}
   <button on:click={handleClick}>Click {count === 0 ? '' : count}</button>
+  <button on:click={toggleDarkMode}>Dark Mode</button>
 </div>
